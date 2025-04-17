6d723218d9b2b80bbbc45a885804525df98e58a1
hyeon min yang <yh9654@gmail.com>
2025-04-17 23:21:35 +0900

    first

---
+++ app/crawler.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
+import os
+from dotenv import load_dotenv
